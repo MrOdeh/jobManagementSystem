8f3fc540cd6eeaa503b1f4ca2742922481971f7c
MrOdeh <mahmoud.odehgs@gmail.com>
2021-07-30 19:04:33 +0300

    initial 01

--- src/main/java/com/payoneer/dev/jobmanagementsystem/ServletInitializer.java
+++ src/main/java/com/payoneer/dev/jobmanagementsystem/ServletInitializer.java
@@ -2,7 +2,7 @@ package com.payoneer.dev.jobmanagementsystem;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+// new defualt config
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
