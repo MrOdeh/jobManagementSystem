487550c568a93d44ef5efbec145a3bff1210a05c
MrOdeh <mahmoud.odehgs@gmail.com>
2021-08-03 13:20:37 +0300

    initial 14

--- src/main/java/com/payoneer/dev/jobmanagementsystem/domain/Job.java
+++ src/main/java/com/payoneer/dev/jobmanagementsystem/domain/Job.java
@@ -1,6 +1,5 @@
 package com.payoneer.dev.jobmanagementsystem.domain;
 
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
---
+++ src/test/java/com/payoneer/dev/jobmanagementsystem/services/impl/EmailJobServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.payoneer.dev.jobmanagementsystem.services.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailJobServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void saveAll() {
+    }
+}
