9ad47d90af6c199388b17b3611b5d2605d86db80
MrOdeh <mahmoud.odehgs@gmail.com>
2021-08-06 21:06:38 +0300

    initial 26

--- src/main/java/com/payoneer/dev/jobmanagementsystem/web/model/EmailJobDto.java
+++ src/main/java/com/payoneer/dev/jobmanagementsystem/web/model/EmailJobDto.java
@@ -2,6 +2,8 @@ package com.payoneer.dev.jobmanagementsystem.web.model;
 
 import lombok.*;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -10,7 +12,17 @@ public class EmailJobDto {
 
     private String messageBody;
 
+    private String messageSubject;
+
     private String sender;
 
-    private String receiver;
+    private String recipients;
+
+    private Boolean isHtml;
+
+    private String attachmentPath;
+
+    private List<String> ccList;
+
+    private List<String> bccList;
 }
